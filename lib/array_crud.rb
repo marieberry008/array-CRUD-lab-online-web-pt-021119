6beb209495ebe7a5209369df862c76f4709fc257
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-27 14:39:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["red", "yellow", "blue", "green"]
-  array.push("black")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
