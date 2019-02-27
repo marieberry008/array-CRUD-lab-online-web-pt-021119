70eef4e5575be34c6a21752185b3a9ba2f3f689a
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-27 14:39:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = ["red", "yellow", "blue", "green"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["red", "yellow", "blue", "green"]
+  array.push("black")
 end
 
 def add_element_to_start_of_array(array, element)
